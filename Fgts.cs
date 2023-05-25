05a019f3ab3ef8f0a14116fc27ab1215060486f1
daniel2003zero <128190991+daniel2003zero@users.noreply.github.com>
2023-05-25 19:18:36 -0300

    Fgts.cs

---
+++ Fgts.cs
@@ -0,0 +1,18 @@
+﻿namespace iRh.Windows.Core
+{
+    public static class Fgts
+    {
+        public static double CalculaFgts(double salario)
+        {
+            double fgtsMensal = CalculaPorcentagemFgts(salario);
+
+            return fgtsMensal;
+        }
+
+        private static double CalculaPorcentagemFgts(double salario)
+        {
+            return salario * 8.0/100;
+        }
+
+    }
+}
